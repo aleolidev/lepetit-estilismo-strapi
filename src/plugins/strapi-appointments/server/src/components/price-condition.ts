b4e44767c7724621664bf2afb25f976b81f752c1
aleolidev <inmortalkaktus@hotmail.com>
2025-04-13 17:52:07 +0200

    refactor: :recycle: remove unnecessary code from priceCondition declaration

--- src/plugins/strapi-appointments/server/src/components/price-condition.ts
+++ src/plugins/strapi-appointments/server/src/components/price-condition.ts
@@ -1,46 +1,24 @@
 export const priceCondition = {
-    collectionName: "components_pricing_price_conditions",
-    info: {
-      displayName: "PriceCondition",
-      icon: "dollar-sign", // Puedes escoger el icono que prefieras
-      description: "Criterio de precio dinámico"
+  collectionName: "components_pricing_price_conditions",
+  info: {
+    displayName: "PriceCondition",
+    icon: "dollar-sign",
+    description: "Criterio de precio dinámico"
+  },
+  options: {},
+  attributes: {
+    criterion: {
+      type: "string",
+      required: true,
     },
-    options: {},
-    attributes: {
-      criterion: {
-        type: "string",
-        required: true,
-      },
-      value: {
-        type: "string",
-        required: true,
-      },
+    value: {
+      type: "string",
+      required: true,
     },
-    __filename__: "price-condition.json",
-    __schema__: {
-      collectionName: "components_custom_price_conditions",
-      info: {
-        displayName: "PriceCondition",
-        icon: "dollar-sign",
-        description: "Criterio de precio dinámico"
-      },
-      options: {},
-      attributes: {
-        criterion: {
-          type: "string",
-          required: true,
-        },
-        value: {
-          type: "string",
-          required: true,
-        },
-      },
-      __filename__: "price-condition.json",
-    },
-    uid: "pricing.price-condition",
-    category: "pricing",
-    modelType: "component",
-    modelName: "price-condition",
-    globalId: "ComponentPricingPriceCondition",
-  };
-  
+  },
+  uid: "pricing.price-condition",
+  category: "pricing",
+  modelType: "component",
+  modelName: "price-condition",
+  globalId: "ComponentPricingPriceCondition",
+};
